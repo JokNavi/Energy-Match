a790059344627720b1c6c91f42521927c2262ad5
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-02-13 13:30:48 +0100

    Added basic swipe up and down functionality

--- src/data/shapes.rs
+++ src/data/shapes.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 
 pub struct Shape {
     pub index: i32,
+    pub rotations: i32,
     pub side: Side,
     pub side_collection: HashMap<i32, Side>,
 }
@@ -11,17 +12,36 @@ impl Shape {
     pub fn new(index: i32) -> Self {
         Shape {
             index,
+            rotations: 0,
             side: Side::new(index),
-            side_collection: HashMap::from([(index, Side::new(index))])
+            side_collection: HashMap::from([(index, Side::new(index))]),
         }
     }
 
-    pub fn get_side(&mut self, index: i32) -> Side {
-        if self.side_collection.contains_key(&index) {
-            self.side_collection[&index]
-        }else {
-            self.side_collection.insert(index, Side::new(index));
+    fn get_side(&mut self, index: i32) -> Side {
+        if let std::collections::hash_map::Entry::Vacant(e) = self.side_collection.entry(index) {
+            e.insert(Side::new(index));
             Side::new(index)
+        } else {
+            self.side_collection[&index]
+        }
+    }
+
+    pub fn swipe_up(&mut self, amount: i32) {
+        let adjusted_index = Self::adjust_index(self.side.index + amount);
+        self.side = self.get_side(adjusted_index);
+    }
+
+    pub fn swipe_down(&mut self, amount: i32) {
+        let adjusted_index = Self::adjust_index(self.side.index - amount);
+        self.side = self.get_side(adjusted_index);
+    }
+
+    fn adjust_index(index: i32) -> i32 {
+        match index {
+            _ if index > crate::SIDE_AMOUNT => index % crate::SIDE_AMOUNT,
+            _ if index <= 0 => index.abs(),
+            _ => index,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use data::shapes::Shape;
 const SIDE_AMOUNT: i32 = 4;
 
 fn main() {
-    println!("Creating new user...");
-    let shape = Shape::new(0);
+    println!("Starting program...");
+    let mut shape = Shape::new(1);
+    println!("{}", shape.side.index);
+    shape.swipe_up(3);
+    println!("{}", shape.side.index);
+    shape.swipe_down(2);
+    println!("{}", shape.side.index);
+    
 }

3e7d0d7d5da39d31f91e28202075b61d1e31d783
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-02-21 15:52:46 +0100

    push

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,4 +1,3 @@
-pub mod games;
 pub mod colors;
 mod visual_objects;
 pub mod patterns;
--- src/data/patterns.rs
+++ src/data/patterns.rs
@@ -1,7 +1,16 @@
 use super::colors::ColoredText;
 
-type TargetPatternItem = (i32, ColoredText);
+pub type TargetPatternItem = (i32, ColoredText);
 
 pub struct TargetPattern{
     pattern: Vec<TargetPatternItem>
+}
+
+impl TargetPattern {
+    pub fn new(min_value) -> Self {
+         Self { pattern } 
+        }
+
+    
+
 }

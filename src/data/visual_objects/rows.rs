2cb234b5dbd7ce7ca60f926188292e1c23b188e3
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-02-25 15:44:43 +0100

    Added get_slice() test

--- src/data/visual_objects/rows.rs
+++ src/data/visual_objects/rows.rs
@@ -1,5 +1,5 @@
 use crate::data::details::indexes::GenerateSlices;
-use crate::data::game_logic::games::LEVEL_SIZE;
+
 
 pub struct Row {
     slices: Vec<i32>,
@@ -9,14 +9,30 @@ pub struct Row {
 impl GenerateSlices for Row {}
 
 impl Row {
-    pub fn new() -> Self {
+    pub fn new(length: i32) -> Self {
         Self {
-            slices: Self::generate_slices(LEVEL_SIZE),
+            slices: Self::generate_slices(length),
             index: 0,
         }
     }
 
     pub fn get_slice(&self, index: usize) -> Option<i32> {
         self.slices.get(index).copied()
+    }
+
+    pub fn set_slice(&mut self, index: usize, value: i32) -> Result<i32, String> {
+
+    }
+}
+
+#[cfg(test)]
+mod TestRow{
+    use super::Row;
+
+    #[test]
+    fn get_slice(){
+        let row = Row::new(50);
+        assert_eq!(row.get_slice(0).unwrap(), row.slices[0]);
+        assert_eq!(row.get_slice(100), None);
     }
 }

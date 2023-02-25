d58866a56d0cc3de4f90ccac5a2d1c9e8dba582a
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-02-25 15:21:51 +0100

    Adjusted patterns.rs to use GenerateSlices

--- src/data/Details/patterns.rs
+++ src/data/Details/patterns.rs
@@ -1,10 +1,10 @@
-use super::indexes::{CorrectRanges, GenerateSlices};
+use super::indexes::{GenerateSlices, CorrectIndex};
 
 pub struct TargetPattern {
     pub pattern: Vec<i32>,
 }
-impl CorrectRanges for TargetPattern {}
 
+impl CorrectIndex for TargetPattern {}
 impl GenerateSlices for TargetPattern{}
 
 impl TargetPattern {
@@ -15,6 +15,24 @@ impl TargetPattern {
     }
 
     pub fn set_pattern(&mut self, new_pattern: Vec<i32>) {
-        self.pattern = new_pattern;
+        self.pattern = new_pattern.iter().map(|x| Self::adjust_rotation(*x) ).collect();
+    }
+}
+
+#[cfg(test)]
+mod TestTartgetPattern {
+    use super::TargetPattern;
+
+    #[test]
+    fn set_pattern() {
+        let mut target_pattern = TargetPattern::new(3);
+        target_pattern.set_pattern(Vec::from([0, 1, 2, 3]));
+        assert_eq!(target_pattern.pattern, Vec::from([0, 1, 2, 3]));
+
+        target_pattern.set_pattern(Vec::from([4, 1, 2, -1]));
+        assert_eq!(target_pattern.pattern, Vec::from([0, 1, 2, 3]));
+
+        target_pattern.set_pattern(Vec::from([0, -5, 2, 3]));
+        assert_eq!(target_pattern.pattern, Vec::from([0, 3, 2, 3]));
     }
 }

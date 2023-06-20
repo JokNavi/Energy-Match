d9b08c6fede83e6cbc41c0825fd7c6f70ccdfae5
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-06-20 19:03:15 +0200

    Add columns_selected_indexes to ColumnWindow

--- energy_match/src/window.rs
+++ energy_match/src/window.rs
@@ -1,14 +1,20 @@
-const DefaultAmountOfColumns: u8 = 11;
-const DefaultColumnHeight: u8 = 8;
-const DefaultWindowHeight: u8 = 3;
-const DefaultRootValueIndex: u8 = 6;
+use rand::prelude::*;
+
+pub type ColumnsSelectedIndexes = Vec<u8>;
+pub type Columns = Vec<Vec<u8>>;
+
+const DEFAULT_COLUMN_AMOUNT: u8 = 11;
+const DEFAULT_COLUMN_HEIGHT: u8 = 8;
+const DEFAULT_WINDOW_HEIGHT: u8 = 3;
+const DEFAULT_ROOT_VALUE_INDEX: u8 = 6;
 
 pub struct ColumnWindow {
-    columns: Vec<Vec<u8>>,
+    columns: Columns,
     column_amount: u8,
     column_height: u8,
     window_height: u8,
     root_value_index: u8,
+    columns_selected_indexes: ColumnsSelectedIndexes,
 }
 
 impl ColumnWindow {
@@ -17,14 +23,18 @@ impl ColumnWindow {
         column_height: u8,
         window_height: u8,
         root_value_index: u8,
+
     ) -> Self {
-        let columns = vec![vec![0; column_height]; column_amount];
+        let columns: Columns = vec![(1..=window_height).collect(); column_amount as usize];
+        let mut rng = thread_rng();
+        let columns_selected_indexes: ColumnsSelectedIndexes = vec![rng.gen_range(0..column_height); column_amount as usize]; //random for now
         Self {
             columns,
             column_amount,
             column_height,
             window_height,
             root_value_index,
+            columns_selected_indexes,
         }
     }
 }
@@ -32,10 +42,10 @@ impl ColumnWindow {
 impl Default for ColumnWindow {
     fn default() -> Self {
         ColumnWindow::new(
-            DefaultAmountOfColumns,
-            DefaultColumnHeight,
-            DefaultColumnHeight,
-            DefaultRootValueIndex,
+            DEFAULT_COLUMN_AMOUNT,
+            DEFAULT_COLUMN_HEIGHT,
+            DEFAULT_WINDOW_HEIGHT,
+            DEFAULT_ROOT_VALUE_INDEX,
         )
     }
 }
